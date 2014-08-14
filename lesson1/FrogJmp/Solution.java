4ba09507d0684605685dd189ddb2414e6a7370e9
Michal Vich <vich@zensys.cz>
2014-08-14 18:11:03 +0200

    solution for FrogJump

--- lesson1/FrogJmp/Solution.java
+++ lesson1/FrogJmp/Solution.java
@@ -1,5 +1,20 @@
 class Solution {
     public int solution(int X, int Y, int D) {
         // write your code in Java SE 8
+
+        if (X>=Y) {
+            return 0;
+        } else if (X+D>=Y) {
+            return 1;
+        }
+
+        int modulo = (Y-X)%D;
+
+        if (modulo==0) {
+            return (Y-X)/D;
+        } else {
+            return ((Y-X)/D)+1;
+        }
+
     }
 }
