2f4daf98124ba738093ce1489d99ab1aed6e8ba8
Michal Vich <vich@zensys.cz>
2014-08-15 08:42:04 +0200

    solution 100% for PermMissingElement

--- lesson1/PermMissingElement/Solution.java
+++ lesson1/PermMissingElement/Solution.java
@@ -1,26 +1,23 @@
-// you can also use imports, for example:
-// import java.util.*;
-
-// you can use System.out.println for debugging purposes, e.g.
-// System.out.println("this is a debug message");
-
 import java.util.Arrays;
 
 class Solution {
     public int solution(int[] A) {
         // write your code in Java SE 8
 
-        Arrays.sort(A);
+        int previousValue = 1;
 
-        for (int i=0; i<A.length-1; i++) {
-
-            if (A[i]+1!=A[i+1]) {
-              return A[i]+1;
+        if (A.length>0) {
+          Arrays.sort(A);
+          for (int i : A) {
+            if (previousValue != i) {
+              return previousValue;
             }
-
+            previousValue++;
+          }
+                    
         }
 
-        return -1;
+        return previousValue;
 
     }
 }
